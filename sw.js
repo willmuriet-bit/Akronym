d0954679bf3d6be3d608c83bbd4096a09109ef0a
Stats <will.muriet@gmail.com>
2026-02-28 11:08:28 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,7 +5,6 @@ const urlsToCache = [
   '/manifest.json'
 ];
 
-// Installation du service worker
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
@@ -16,7 +15,6 @@ self.addEventListener('install', event => {
   );
 });
 
-// Activation du service worker
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(cacheNames => {
@@ -31,35 +29,25 @@ self.addEventListener('activate', event => {
   );
 });
 
-// Interception des requêtes
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        // Cache trouvé - le retourner
         if (response) {
           return response;
         }
-        // Cloner la requête
         var fetchRequest = event.request.clone();
-
         return fetch(fetchRequest).then(response => {
-          // Vérifier si réponse valide
           if(!response || response.status !== 200 || response.type !== 'basic') {
             return response;
           }
-
-          // Cloner la réponse
           var responseToCache = response.clone();
-
           caches.open(CACHE_NAME)
             .then(cache => {
               cache.put(event.request, responseToCache);
             });
-
           return response;
         }).catch(() => {
-          // Si offline, retourner index.html pour les routes
           if (event.request.mode === 'navigate') {
             return caches.match('/index.html');
           }
