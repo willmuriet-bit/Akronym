a19e8e0ab231cfdf12ad2a88ba32c8ff115fa6cc
Stats <will.muriet@gmail.com>
2026-02-28 10:46:07 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,3 @@
+const CACHE_NAME = 'akronym-v1';
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME)));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
